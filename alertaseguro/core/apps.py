66aecd97ab2678da9addb190e0be41b7e7747dca
Fabio Barros <5477@eclisboa.net>
2026-03-01 18:58:00 +0000

    1/03 v3

--- alertaseguro/core/apps.py
+++ alertaseguro/core/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
-from .scheduler import start
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
 
     def ready(self):
+        from .scheduler import start
         start()
