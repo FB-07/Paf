365f7d1f77321992bc4ff1a7e5253bc879ba7c9c
Fabio Barros <5477@eclisboa.net>
2026-03-01 18:46:43 +0000

    1/03 v2

--- alertaseguro/core/scheduler.py
+++ alertaseguro/core/scheduler.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.utils import timezone
 from core.services.ipma_service import update_ipma_warnings
-
 from core.services.api_importer import fetch_and_save
 
 
@@ -16,8 +15,18 @@ def start():
         replace_existing=True,
         max_instances=1,
         coalesce=True,
+        next_run_time=timezone.now(), 
     )
 
-    scheduler.add_job(update_ipma_warnings, "interval", minutes=10)
+    scheduler.add_job(
+        update_ipma_warnings,
+        "interval",
+        minutes=20,
+        id="update_ipma_20min",
+        replace_existing=True,
+        max_instances=1,
+        coalesce=True,
+        next_run_time=timezone.now(),
+    )
 
     scheduler.start()
