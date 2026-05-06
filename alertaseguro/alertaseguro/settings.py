a72808a2308e69603fe4acf16586fd851efb3178
Fabio Barros <5477@eclisboa.net>
2026-05-06 18:06:55 +0100

    fix: Update email configuration to use Gmail SMTP settings

--- alertaseguro/alertaseguro/settings.py
+++ alertaseguro/alertaseguro/settings.py
@@ -25,9 +25,11 @@ CSRF_TRUSTED_ORIGINS = [
 #CRON_TOKEN = "cron_token_@l3rt3s3gur0"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
