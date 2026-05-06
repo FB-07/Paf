bdd4170f8b46dbb3819302faf85f60ab32e9ee43
Fabio Barros <5477@eclisboa.net>
2026-05-06 14:34:42 +0100

    feat: Update email configuration to use environment variables for sensitive data

--- alertaseguro/alertaseguro/settings.py
+++ alertaseguro/alertaseguro/settings.py
@@ -28,8 +28,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alertaseguropt@gmail.com'
-EMAIL_HOST_PASSWORD = 'xyly aaay diiz utci'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "alertaseguropt@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "xyly aaay diiz utci")
 
 # =========================
 # Apps
